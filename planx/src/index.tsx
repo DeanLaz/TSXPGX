8d815da1a0f1cb0c7d24d53686914ffa329c3c70
DeanLaz <DeanLazx@gmail.com>
2020-11-05 22:02:45 -0800

    Redux Setup

--- planx/src/index.tsx
+++ planx/src/index.tsx
@@ -1,29 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers";
+const store = createStore(reducers, applyMiddleware(thunk));
 
-interface AppProps {
-  color?: string;
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
+// CLASS COMPONENT =========
+// interface AppProps {
+//     color?: string;
+//   }
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 };
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>+</button>
-        <button onClick={this.onDecrement}>-</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
 
-ReactDOM.render(<App color="red" />, document.querySelector("#root"));
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>+</button>
+//         <button onClick={this.onDecrement}>-</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+// FUNCTIONAL COMPONENT ==========
+
+// interface AppProps {
+//     color?: string;
+//   }
+
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
+
+// ReactDOM.render(<App color="red" />, document.querySelector("#root"));
