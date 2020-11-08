eb4be10687ac278404049092e3ff1260d035c6ef
DeanLaz <DeanLazx@gmail.com>
2020-11-07 16:02:33 -0800

    Type Definition Files + Loading Msg

--- planx/src/components/App.tsx
+++ planx/src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { StoreState } from "../reducers";
 import { Todo, fetchTodos, deleteTodo } from "../actions";
+import { StoreState } from "../reducers";
 
 interface AppProps {
   todos: Todo[];
